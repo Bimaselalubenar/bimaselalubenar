df2aa16543dc41d6a5fea6dffa83995c0a86cb38
blackhole <bimaardiantopramana@gmail.com>
2024-08-18 19:17:28 +0700

    asdasd

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -43,4 +43,10 @@ document.addEventListener('DOMContentLoaded', function() {
 			}
 		})
 	}
+
+	var charts = document.querySelectorAll('[data-bss-chart]');
+
+	for (var chart of charts) {
+		chart.chart = new Chart(chart, JSON.parse(chart.dataset.bssChart));
+	}
 }, false);
