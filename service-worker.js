e1855b2df78a5e458932b6eb9423f34900c7f8ae
blackhole <bimaardiantopramana@gmail.com>
2024-10-12 08:48:11 +0700

    asdasdasd

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-// Daftar aset yang akan di-cache
 const CACHE_NAME = 'offline-cache';
 const OFFLINE_URL = 'offline.html';
+const BOOTSTRAP_CSS = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css';
 
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
-            return cache.addAll([OFFLINE_URL]);
+            return cache.addAll([OFFLINE_URL, BOOTSTRAP_CSS]); // Cache halaman offline dan CSS Bootstrap
         })
     );
 });
@@ -15,6 +15,12 @@ self.addEventListener('fetch', function (event) {
         event.respondWith(
             fetch(event.request).catch(function () {
                 return caches.match(OFFLINE_URL);
+            })
+        );
+    } else if (event.request.url.includes('bootstrap.min.css')) {
+        event.respondWith(
+            caches.match(BOOTSTRAP_CSS).then(function (response) {
+                return response || fetch(BOOTSTRAP_CSS);
             })
         );
     }
